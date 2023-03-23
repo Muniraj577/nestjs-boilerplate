df508ba88b5610d369f6dad2becccd21af63143a
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2023-03-23 10:06:58 +0545

    migration file added and column added

--- src/migrations/1679544670546-CreateUserTable.ts
+++ src/migrations/1679544670546-CreateUserTable.ts
@@ -1,11 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUserTable1679544670546 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+              name: 'users',
+              columns:[
+                  {
+                      name: 'id',
+                      type: 'int',
+                      isPrimary: true,
+                      isGenerated: true,
+                      generationStrategy: 'increment'
+                  },
+                  {
+                      name: 'name',
+                      type: 'varchar',
+                  }
+              ]
+          }),
+          true,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
     }
 
 }
